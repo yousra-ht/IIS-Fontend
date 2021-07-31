cfdf79eddb0ddd6ea534be2b71385f5fbd4e50c5
yousra-ht <yousrahajtaieb@gmail.com>
2021-07-31 14:37:57 +0200

    31/07

---
+++ src/app/BlogFolder/detailed-article/detailed-article.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detailed-article',
+  templateUrl: './detailed-article.component.html',
+  styleUrls: ['./detailed-article.component.css']
+})
+export class DetailedArticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
