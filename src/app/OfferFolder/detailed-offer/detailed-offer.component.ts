64c7021fb92af9c4171ea3c28210fe32fcc2dc99
Molka <molkabenameur6@gmail.com>
2021-08-18 15:56:41 +0200

    18/08

--- src/app/OfferFolder/detailed-offer/detailed-offer.component.ts
+++ src/app/OfferFolder/detailed-offer/detailed-offer.component.ts
@@ -23,9 +23,13 @@ export class DetailedOfferComponent implements OnInit {
   constructor(private _offerService: OfferService,private route: ActivatedRoute ) { }
 
   ngOnInit() {
-    this._offerService.getAlloffer().then((resp) => { this.OffreListe= resp.data ; console.log(this.OffreListe) });
+    this._offerService.getAlloffer().then((resp) => { this.OffreListe= resp.data ;
+      //  console.log(this.OffreListe) 
+      });
     const id = this.route.snapshot.paramMap.get("id");
-    this._offerService.getoffer(id).then((resp) => { this.Candidature = resp.data; console.log(this.Candidature) });
+    this._offerService.getoffer(id).then((resp) => { this.Candidature = resp.data
+           console.log(this.Candidature)
+       });
   }
 
   elem;
@@ -36,28 +40,74 @@ export class DetailedOfferComponent implements OnInit {
       cv: this.elem.files[0]
     });
     this.form.get('cv').updateValueAndValidity()
-  }
+   }
 
-  submit(e) {
+
+  
+   submit(e) {
     e.stopPropagation()
     {
-      var formData: any = new FormData();
-      formData.append("cv", this.elem.files[0]);
-      formData.append("nom", this.form.get('nom').value);
-      formData.append("prenom", this.form.get('prenom').value);
-      formData.append("email", this.form.get('email').value);
-      this._offerService.createcandidature(
-        formData
-      ).then(() => 
-      {
+      const id = this.route.snapshot.paramMap.get("id");
+      this._offerService.getoffer(id).then((resp) => { 
+                this.Candidature = resp.data; 
+                console.log(this.Candidature)
+              
+                var formData: any = new FormData();
+                formData.append("cv", this.elem.files[0]);
+                formData.append("nom", this.form.get('nom').value);
+                formData.append("prenom", this.form.get('prenom').value);
+                formData.append("email", this.form.get('email').value);
+                formData.append("offreEmplois", "/api/offre_emplois/" +id );  
 
-        console.warn("success");
-        // this.router.navigate(['/Partenaire']);
+                
 
-      })
+                console.log(formData.values())
+
+
+
+
+
+                this._offerService.createcandidature(
+                  formData
+                 
+                ).then(() => 
+                {
+          
+                  console.warn("success");
+                  // this.router.navigate(['/Partenaire']);
+          
+                })
+
+              });
     }
-
   }
+
+
+
+  
+ 
+
+
+  // submit(e) {
+  //   e.stopPropagation()
+  //   {
+  //     var formData: any = new FormData();
+  //     formData.append("cv", this.elem.files[0]);
+  //     formData.append("nom", this.form.get('nom').value);
+  //     formData.append("prenom", this.form.get('prenom').value);
+  //     formData.append("email", this.form.get('email').value);
+  //     this._offerService.createcandidature(
+  //       formData
+  //     ).then(() => 
+  //     {
+
+  //       console.warn("success");
+  //       // this.router.navigate(['/Partenaire']);
+
+  //     })
+  //   }
+
+  // }
 
   // console.warn(this.formPart.get('nom').value);
   // console.log(this.formPartenaire.get('logo').value);
