4b9f0ceb75ebd6d4f8d5b0d8a1963c67bd998176
yousra-ht <yousrahajtaieb@gmail.com>
2021-09-02 16:29:22 +0200

    16/29

--- src/app/BlogFolder/detailed-article/detailed-article.component.ts
+++ src/app/BlogFolder/detailed-article/detailed-article.component.ts
@@ -4,33 +4,8 @@ import { BlogService } from '../blog.service';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
-@Component({
-  selector: 'ngbd-modal-content',
-  template: `
-    <div class="modal-header">
-      <h4 class="modal-title">Hi there!</h4>
-      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-    <div class="modal-body">
-      <p>Hello, {{name}}!</p>
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
-    </div>
-  `
-})
-export class NgbdModalContent {
-  @Input() name;
 
-  constructor() { }
-}
-@Component({
-  selector: 'app-detailed-article',
-  templateUrl: './detailed-article.component.html',
-  styleUrls: ['./detailed-article.component.css']
-})
+
 
 export class DetailedArticleComponent implements OnInit {
   form: FormGroup;
@@ -77,7 +52,7 @@ export class DetailedArticleComponent implements OnInit {
         'email': this.form.get('email').value,
         'telephone': "null",
         'password': "null",
-  
+
       }).then(() => {
 
         this.blogService.getMember(this.form.get('email').value).then((resp) => {
